7b413ce2b7bb6996e1c8819bcd96e7d941ff1656
JuanManuelSanjurjo <juanmanuelsanjurjo@hotmail.com>
2024-03-08 22:43:24 -0300

    added view transitions}

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://marvelous-manatee-0d8623.netlify.app",
+  integrations: [preact()]
+});
---
+++ src/components/ReactTestComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useState } from 'react';
+function ReactTestComponent() {
+  return (
+    <ul className='toto'>
+      <li>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 3</li>
+    </ul>
+  )
+}
+
+export default ReactTestComponent
+
